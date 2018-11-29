0a5bae406a2d2292d66470350de9b4f3604900d2
herferre <herve.ferreira@coriant.com>
2018-11-29 11:35:21 +0000

    Add discover service

---
+++ discoverservice/src/main/java/com/pharma/home/discoverservice/DiscoverserviceApplication.java
@@ -0,0 +1,12 @@
+package com.pharma.home.discoverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverserviceApplication.class, args);
+    }
+}
