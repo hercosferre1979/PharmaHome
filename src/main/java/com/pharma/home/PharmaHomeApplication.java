779a81c91b194309968206bfec2417594d9b9097
herferre <herve.ferreira@coriant.com>
2018-11-29 11:16:09 +0000

    Initial commit

---
+++ src/main/java/com/pharma/home/PharmaHomeApplication.java
@@ -0,0 +1,12 @@
+package com.pharma.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaHomeApplication.class, args);
+    }
+}
