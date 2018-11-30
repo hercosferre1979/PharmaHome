53d96b1e9662f2c4997d8f2f8e77bd7a7e6442ea
herferre <herve.ferreira@coriant.com>
2018-11-30 14:53:43 +0000

    Add Admin Service

--- registerservice/src/main/java/com/pharma/home/registerservice/RegisterServiceApplication.java
+++ registerservice/src/main/java/com/pharma/home/registerservice/RegisterServiceApplication.java
@@ -1,5 +1,7 @@
 package com.pharma.home.registerservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,12 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RegisterServiceApplication {
 
+    private final Logger logger = LoggerFactory.getLogger(RegisterServiceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(RegisterServiceApplication.class, args);
     }
 
     @RequestMapping("/registerpharmacie")
     Boolean registerPharmacie(String name) {
+
+        for (int i = 0; i < 2; i++) {
+            logger.info("Name {} iteration {} this {}", name, i, this);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         if (name.contains("bla")) {
             return true;
         } else {
