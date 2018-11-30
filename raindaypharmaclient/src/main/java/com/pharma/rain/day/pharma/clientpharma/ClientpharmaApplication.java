768a1cd0c00ca8e1d72986a45c500597c473cb7f
herferre <herve.ferreira@coriant.com>
2018-11-30 11:58:18 +0000

    Add Logback

--- raindaypharmaclient/src/main/java/com/pharma/rain/day/pharma/clientpharma/ClientpharmaApplication.java
+++ raindaypharmaclient/src/main/java/com/pharma/rain/day/pharma/clientpharma/ClientpharmaApplication.java
@@ -1,5 +1,7 @@
 package com.pharma.rain.day.pharma.clientpharma;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @RestController
 public class ClientpharmaApplication {
+
+    private final Logger logger = LoggerFactory.getLogger(ClientpharmaApplication.class);
 
     @Autowired
     private RegisterService registerService;
@@ -28,6 +33,7 @@ public class ClientpharmaApplication {
             path = "/clientRegisterPharma"
     )
     public Boolean registerPharmacie(@RequestParam("pharmacieName") String pharmacieName) {
+        logger.info("New request!!!!!!!!!!!!!!!!");
         return registerService.registerPharmacie(pharmacieName);
     }
 }
